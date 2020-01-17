57672b0d3cb6bd6f136d96eb29e3379142aa0c20
sounisi5011 <sounisi5011@users.noreply.github.com>
2020-01-18 06:42:32 +0900

    ✅ Fix Packrat Parsing test code

--- test/packrat-parsing.ts
+++ test/packrat-parsing.ts
@@ -1,14 +1,42 @@
 import test from 'ava';
+import { performance, PerformanceEntry, PerformanceObserver } from 'perf_hooks';
+import * as sta from 'simple-statistics';
 
 import p from '../src';
 
-test('should parse arithmetics', t => {
-    t.timeout(1000);
+function isLinearity(
+    data: number[],
+    standardDeviationThreshold: number,
+): boolean {
+    const diffs = data.reduce<number[]>(
+        (diffs, value, index, data) =>
+            index > 0 ? [...diffs, value - data[index - 1]] : [],
+        [],
+    );
+    return sta.standardDeviation(diffs) <= standardDeviationThreshold;
+}
 
+test('should use Packrat Parsing', t => {
     /** @see https://engineering.linecorp.com/ja/blog/peg-parser-generator-packrat-parser/ */
     const S = p.seq(() => [A]); // eslint-disable-line @typescript-eslint/no-use-before-define
     const A = p.or(() => [p.seq(P, '+', A), p.seq(P, '-', A), P]); // eslint-disable-line @typescript-eslint/no-use-before-define
     const P = p.or(p.seq('(', A, ')'), '1');
 
-    t.log(S.parse('(((((((((((((1)))))))))))))'));
+    const parse = performance.timerify(S.parse.bind(S));
+
+    const entries: PerformanceEntry[] = [];
+    const obs = new PerformanceObserver(list => {
+        entries.push(...list.getEntries());
+    });
+    obs.observe({ entryTypes: ['function'] });
+
+    for (const i of Array(15).keys()) {
+        parse('('.repeat(i) + '1' + ')'.repeat(i));
+    }
+
+    const durations = entries.map(ent => ent.duration);
+
+    t.log(entries);
+    t.log({ durations });
+    t.true(isLinearity(durations, 100));
 });
