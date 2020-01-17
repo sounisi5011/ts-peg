fbe64f4741ab63b7cad4018400c5272180626ca1
sounisi5011 <sounisi5011@users.noreply.github.com>
2020-01-18 00:15:59 +0900

    🎨 Change grammar of action
    Changed to use action() method instead of second argument.

--- examples/arithmetics.grammar.ts
+++ examples/arithmetics.grammar.ts
@@ -7,45 +7,43 @@ import p from '../src';
 
 /* eslint-disable @typescript-eslint/no-use-before-define */
 
-export const Expression = p.seq(
-    () => [
+export const Expression = p
+    .seq(() => [
         Term,
-        p.zeroOrMore(
-            [ws, p.or('+', '-'), ws, Term],
-            ([, operator, , factor]) => [operator, factor],
-        ),
-    ],
-    ([head, tail]) =>
+        p
+            .zeroOrMore(ws, p.or('+', '-'), ws, Term)
+            .action(([, operator, , factor]) => [operator, factor]),
+    ])
+    .action(([head, tail]) =>
         tail.reduce((result, [operator, factor]) => {
             if (operator === '+') return result + factor;
             if (operator === '-') return result - factor;
         }, head),
-);
+    );
 
-export const Term = p.seq(
-    () => [
+export const Term = p
+    .seq(() => [
         Factor,
-        p.zeroOrMore(
-            [ws, p.or('*', '/'), ws, Factor],
-            ([, operator, , factor]) => [operator, factor],
-        ),
-    ],
-    ([head, tail]) =>
+        p
+            .zeroOrMore(ws, p.or('*', '/'), ws, Factor)
+            .action(([, operator, , factor]) => [operator, factor]),
+    ])
+    .action(([head, tail]) =>
         tail.reduce((result, [operator, factor]) => {
             if (operator === '*') return result * factor;
             if (operator === '/') return result / factor;
         }, head),
-);
+    );
 
 export const Factor = p.or(() => [
-    p.seq(['(', ws, Expression, ws, ')'], ([, , exp]) => exp),
+    p.seq('(', ws, Expression, ws, ')').action(([, , exp]) => exp),
     Integer,
 ]);
 
-export const Integer = p.label('integer').seq(
-    () => [ws, p.range('0', '9').oneOrMore],
-    ({ text }) => parseInt(text, 10),
-);
+export const Integer = p
+    .label('integer')
+    .seq(() => [ws, p.range('0', '9').oneOrMore])
+    .action((_, { text }) => parseInt(text, 10));
 
 export const ws = p.label('whitespace').or(...' \t\n\r').zeroOrMore;
 
