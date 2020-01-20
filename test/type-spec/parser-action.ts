a9f8abb3fcbf2f23a1d3bfe8d203643436e3ad0f
sounisi5011 <sounisi5011@users.noreply.github.com>
2020-01-20 14:41:17 +0900

    ✅ Add test for type definition of exp.action() method

--- test/type-spec/parser-action.ts
+++ test/type-spec/parser-action.ts
@@ -40,3 +40,71 @@ p.seq(p.chars('A-Z'), '-', p.chars('0-9')).action(exps => {
     const parser = p.seq('1', '2', '3').action(nums => nums.map(Number));
     assertType<TypeEq<typeof parser, Parser<number[]>>>();
 }
+{
+    const parser = p.str('null').action(() => null);
+    assertType<TypeEq<typeof parser, Parser<null>>>();
+}
+{
+    const parser = p.str('void').action(() => undefined);
+    assertType<TypeEq<typeof parser, Parser<undefined>>>();
+}
+{
+    const parser = p.str('true').action(() => true);
+    assertType<TypeEq<typeof parser, Parser<boolean>>>();
+}
+{
+    const parser = p.str('true').action(() => true as const);
+    assertType<TypeEq<typeof parser, Parser<true>>>();
+}
+{
+    const parser = p.str('false').action(() => false);
+    assertType<TypeEq<typeof parser, Parser<boolean>>>();
+}
+{
+    const parser = p.str('false').action(() => false as const);
+    assertType<TypeEq<typeof parser, Parser<false>>>();
+}
+{
+    const parser = p.str('-1').action(() => -1);
+    assertType<TypeEq<typeof parser, Parser<number>>>();
+}
+{
+    const parser = p.str('-1').action(() => -1 as const);
+    assertType<TypeEq<typeof parser, Parser<-1>>>();
+}
+{
+    const parser = p.str('0').action(() => 0);
+    assertType<TypeEq<typeof parser, Parser<number>>>();
+}
+{
+    const parser = p.str('0').action(() => 0 as const);
+    assertType<TypeEq<typeof parser, Parser<0>>>();
+}
+{
+    const parser = p.str('1').action(() => 1);
+    assertType<TypeEq<typeof parser, Parser<number>>>();
+}
+{
+    const parser = p.str('1').action(() => 1 as const);
+    assertType<TypeEq<typeof parser, Parser<1>>>();
+}
+{
+    const parser = p.str('a').action(() => 'a');
+    assertType<TypeEq<typeof parser, Parser<string>>>();
+}
+{
+    const parser = p.str('a').action(() => 'a' as const);
+    assertType<TypeEq<typeof parser, Parser<'a'>>>();
+}
+{
+    const parser = p.str('f').action(() => 'foo');
+    assertType<TypeEq<typeof parser, Parser<string>>>();
+}
+{
+    const parser = p.str('f').action(() => 'foo' as const);
+    assertType<TypeEq<typeof parser, Parser<'foo'>>>();
+}
+{
+    const parser = p.str('@@iterator').action(() => Symbol.iterator);
+    assertType<TypeEq<typeof parser, Parser<symbol>>>();
+}
