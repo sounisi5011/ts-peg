fb769814f99cdd6bda15b04dd431d9a09cc4b8f3
sounisi5011 <sounisi5011@users.noreply.github.com>
2020-01-21 23:24:26 +0900

    ✅ Update test for type definition of exp.times() method
    Added tests for union types.

--- test/type-spec/parser-props.ts
+++ test/type-spec/parser-props.ts
@@ -60,11 +60,27 @@ import p, { Parser } from '../../src';
     assertType<TypeEq<typeof parser, Parser<string[]>>>();
 }
 {
+    const parser = p.any.times(NaN);
+    assertType<TypeEq<typeof parser, Parser<string[]>>>();
+}
+{
+    const parser = p.any.times(Infinity);
+    assertType<TypeEq<typeof parser, Parser<string[]>>>();
+}
+{
     const parser = p.str('x').times(2);
     assertType<TypeEq<typeof parser, Parser<['x', 'x']>>>();
 }
 {
     const parser = p.str('x').times(Math.PI);
+    assertType<TypeEq<typeof parser, Parser<'x'[]>>>();
+}
+{
+    const parser = p.str('x').times(NaN);
+    assertType<TypeEq<typeof parser, Parser<'x'[]>>>();
+}
+{
+    const parser = p.str('x').times(Infinity);
     assertType<TypeEq<typeof parser, Parser<'x'[]>>>();
 }
 {
@@ -94,4 +110,43 @@ import p, { Parser } from '../../src';
 {
     const parser = p.seq('0', '1').times(process.pid);
     assertType<TypeEq<typeof parser, Parser<['0', '1'][]>>>();
+}
+{
+    function random(): 0 | 1 | 2 {
+        const rand = Math.random() * 3;
+        if (rand >= 2) {
+            return 2;
+        } else if (rand >= 1) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    const parser = p.str('x').times(random());
+    parser.action(([v1, v2]) => {
+        assertType<TypeEq<typeof v1, 'x' | undefined>>();
+        assertType<TypeEq<typeof v2, 'x' | undefined>>();
+    });
+}
+{
+    function random(): 1 | 2 | 9 {
+        const rand = Math.random() * 3;
+        if (rand >= 2) {
+            return 9;
+        } else if (rand >= 1) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
+    const parser = p.str('x').times(random());
+    parser.action(value => {
+        assertType<TypeEq<typeof value[0], 'x'>>();
+        assertType<TypeEq<typeof value[1], 'x' | undefined>>();
+        if (value.length === 9) {
+            assertType<TypeEq<typeof value[2], 'x'>>();
+            assertType<TypeEq<typeof value[5], 'x'>>();
+            assertType<TypeEq<typeof value[8], 'x'>>();
+        }
+    });
 }
