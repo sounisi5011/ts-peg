e38a2557c0120ccfc2c6a28771faa182a68d01ee
sounisi5011 <sounisi5011@users.noreply.github.com>
2020-01-30 02:40:23 +0900

    ⏪ Revert "🎨 Update example parser: Off-side rule (Python like indentation)"
    This reverts commit 7c5eab9045389e2a7fa255adc80a0fea4092ec04.

--- examples/python-like-indentation.grammar.ts
+++ examples/python-like-indentation.grammar.ts
@@ -8,7 +8,7 @@ import p, { Parser } from '../src';
 const indentStack: string[] = [];
 let currentIndent = '';
 
-export type Line = string | Line[];
+export type Line = Record<string, Line[] | undefined> | string;
 
 /* eslint-disable @typescript-eslint/no-use-before-define */
 
@@ -18,24 +18,25 @@ const _init = p.str('').action(() => {
 });
 
 export const start = p
-    .seq(() => [_init, INDENT.optional, line.zeroOrMore])
+    .seq(() => [_init, INDENT.optional, line])
     .action(([, , l]) => l);
 
 export const line: Parser<Line> = p
     .seq(() => [
-        p.or(
-            p.seq(INDENT, line.zeroOrMore, DEDENT).action(([, lines]) => lines),
-            p
-                .seq(SAMEDENT, p.seq(p.not_a(EOL), p.any).oneOrMore.text)
-                .action(([, label]) => label),
-        ),
+        SAMEDENT,
+        p.seq(p.not_a(EOL), p.any).action(([, c]) => c).oneOrMore.text,
         EOL.optional,
+        p.seq(INDENT, line.zeroOrMore, DEDENT).action(([, c]) => c).optional,
     ])
-    .action(([list]) => list);
+    .action(([, line, , children]) => {
+        const o: Extract<Line, object> = {};
+        o[line] = children;
+        return children ? o : line;
+    });
 
 export const EOL = p.or('\r\n', '\n', '\r');
 
-export const SAMEDENT = p
+export const SAMEDENT: Parser<string> = p
     .chars(' \t')
     .zeroOrMore.text.match(indent => indent === currentIndent);
 
